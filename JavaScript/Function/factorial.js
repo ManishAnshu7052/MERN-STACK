6d0b8446b22942fd788eba8bc5951dedf7c3f4dd
Manish <patelmanish5272@gmail.com>
2022-12-29 04:35:21 -0800

    factorial

---
+++ JavaScript/Function/factorial.js
@@ -0,0 +1,10 @@
+function getFactorial(num){
+    if(num==1){
+      return 1;
+    }
+    if(num==0 || num<0){
+        return 0;
+    }
+    return num*getFactorial(num-1);
+}
+console.log(getFactorial(5));
