1949526b843a89e78fea0715f86475c3fa3934d1
Manish <patelmanish5272@gmail.com>
2022-12-30 06:34:50 -0800

    padStart and padEnd Method

---
+++ JavaScript/String/pad.js
@@ -0,0 +1,13 @@
+//padStart() method pads a string with another string
+
+const text = "5";
+console.log(text.padStart(4,"x"));
+
+const text2 = "5";
+console.log(text2.padStart(4,"0"));
+//Note-The padStard() method is a string method
+
+//padEnd() method pads a string with another string
+
+const text3 = "5";
+console.log(text3.padEnd(4,"x"));
