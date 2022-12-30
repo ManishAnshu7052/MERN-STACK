b81fc220ba7207e64893ace228aa952768307e46
Manish <patelmanish5272@gmail.com>
2022-12-30 06:18:00 -0800

    Concat

---
+++ JavaScript/String/concat.js
@@ -0,0 +1,10 @@
+// Concat() join two or more string
+
+const text1 = "Hello";
+const text2 = "World";
+console.log(text1.concat(" ",text2));
+
+//The concat() method can be used instead of plus operator
+text = "Hello"+" "+"World";
+text = "Hello".concat(" ","World");
+console.log(text);
