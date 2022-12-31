981e4dc8a7e6fcd744349ca81f6dbf7aee18aa25
Manish <patelmanish5272@gmail.com>
2022-12-30 19:39:13 -0800

    String Search

---
+++ JavaScript/String/indexOf.js
@@ -0,0 +1,6 @@
+/**The indexOf() method returns the index of(the position of) the first
+ * accurance of a apecified text in a string
+ */
+
+const str = "Please locate where locate access";
+console.log(str.indexOf("locate"));
