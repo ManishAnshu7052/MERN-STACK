3a5a1ffd120e0903f11453356490d6f1604992a2
Manish <patelmanish5272@gmail.com>
2022-12-31 01:05:56 -0800

    Search String

---
+++ JavaScript/String/search.js
@@ -0,0 +1,5 @@
+//The search() method search a string for a specified vale and returns the 
+//position of the match:
+
+const str = "Please locate where locate occurs";
+console.log(str.search("locate"));
