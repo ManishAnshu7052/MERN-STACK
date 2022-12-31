cc931dca10515a023af86f2cfbb95caabc325ec1
Manish <patelmanish5272@gmail.com>
2022-12-31 01:29:54 -0800

    Check String

---
+++ JavaScript/String/Questions/Qstring1.js
@@ -0,0 +1,11 @@
+//Write a JavaScript function to check whether an `input` is a string or not
+
+function str(){
+    const variable = 'Hello World';
+    if(typeof variable === 'string'){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+}
+str();
