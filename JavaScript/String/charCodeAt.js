eee225145e7b26963796c257416df4e60ac5f3b6
Manish <patelmanish5272@gmail.com>
2022-12-30 19:30:54 -0800

    Extracting String Character

---
+++ JavaScript/String/charCodeAt.js
@@ -0,0 +1,7 @@
+/**The charCodeAt() method returns the unicode of the character at a specified
+ * index in a string.
+ * The method returns a UTF-16 code (an integer between 0 and 65535)
+ */
+
+const text = "Hello World!";
+console.log(text.charCodeAt(0));
