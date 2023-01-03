f77b70a2e67e7143c4129634bb0b1e9b7903e187
Manish <patelmanish5272@gmail.com>
2023-01-03 04:45:30 -0800

    Round

---
+++ JavaScript/Math/round.js
@@ -0,0 +1,8 @@
+//round() return the nearest integer
+
+const number1 = 4.6;
+const number2 = 4.5;
+const number3 = 4.4;
+console.log(Math.round(number1));//5
+console.log(Math.round(number2));//5
+console.log(Math.round(number3));//4
