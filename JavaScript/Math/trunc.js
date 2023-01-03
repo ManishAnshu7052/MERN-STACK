0cf73eaf216872b6378f269ec94db59232bfbb93
Manish <patelmanish5272@gmail.com>
2023-01-03 05:01:56 -0800

    trunc

---
+++ JavaScript/Math/trunc.js
@@ -0,0 +1,11 @@
+//Math.trunc(x) return the integer part of x
+const num1 = 4.9;
+const num2 = 4.7;
+const num3 = 4.4;
+const num4 = 4.2;
+const num5 = -4.2;
+console.log(Math.trunc(num1));//4
+console.log(Math.trunc(num2));//4
+console.log(Math.trunc(num3));//4
+console.log(Math.trunc(num4));//4
+console.log(Math.trunc(num5));//-4
