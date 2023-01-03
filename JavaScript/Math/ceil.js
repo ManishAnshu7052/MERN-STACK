eb5092d9586f8af3d986f7aa49dc065d0928d8ed
Manish <patelmanish5272@gmail.com>
2023-01-03 04:51:57 -0800

    ceil

---
+++ JavaScript/Math/ceil.js
@@ -0,0 +1,11 @@
+//Math.ceil(x) return the value of x round up to its nearest integer
+const num1 = 4.9;
+const num2 = 4.7;
+const num3 = 4.4;
+const num4 = 4.2;
+const num5 = -4.2;
+console.log(Math.ceil(num1));//5
+console.log(Math.ceil(num2));//5
+console.log(Math.ceil(num3));//5
+console.log(Math.ceil(num4));//5
+console.log(Math.ceil(num5));//-4
