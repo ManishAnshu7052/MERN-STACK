fbd98303d2702bffabe24106e28f87014e4e31e9
Manish <patelmanish5272@gmail.com>
2023-01-03 05:21:45 -0800

    max_min

---
+++ JavaScript/Math/min_max.js
@@ -0,0 +1,6 @@
+//Math.min() Math.max() can be used to find the lowest and largest value in a list of the argument
+const x = (0,150,30,20,-8,-200);
+console.log(Math.min(x));//-200
+
+const y = (0,150,30,20,-8,-200);
+console.log(Math.max(y));//-200
