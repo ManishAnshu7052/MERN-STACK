8f8c2b264ba550476ea2b8dd67243e31bbcf4f44
Manish <patelmanish5272@gmail.com>
2023-01-03 05:08:25 -0800

    sign

---
+++ JavaScript/Math/sign.js
@@ -0,0 +1,9 @@
+//Math.sign(x) return if x is negative , null or positive 
+
+const num1 = -4;
+const num2 = 0;
+const num3 = 4;
+
+console.log(Math.sign(num1));//-1
+console.log(Math.sign(num2));//0
+console.log(Math.sign(num3));//4
