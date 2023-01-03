161fb3e3232d35f747d8b61a2873403a133a18f6
Manish <patelmanish5272@gmail.com>
2023-01-03 04:57:20 -0800

    floor

---
+++ JavaScript/Math/floor.js
@@ -0,0 +1,12 @@
+//Math.floor(x) return the value of x rounded down to its nearest integer
+
+const num1 = 4.9;
+const num2 = 4.7;
+const num3 = 4.4;
+const num4 = 4.2;
+const num5 = -4.2;
+console.log(Math.floor(num1));//4
+console.log(Math.floor(num2));//4
+console.log(Math.floor(num3));//4
+console.log(Math.floor(num4));//4
+console.log(Math.floor(num5));//-5
