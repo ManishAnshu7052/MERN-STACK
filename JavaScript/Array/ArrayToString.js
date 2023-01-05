c05239c019b91c3f5f177dfa8289beaa146cd421
Manish <patelmanish5272@gmail.com>
2023-01-05 05:52:23 -0800

    ArrayToString

---
+++ JavaScript/Array/ArrayToString.js
@@ -0,0 +1,6 @@
+//toString() converts an array to string
+
+const fruits = ["Banana","Orange","Apple","Mango"];
+console.log(fruits);//[ 'Banana', 'Orange', 'Apple', 'Mango' ]
+console.log(fruits.toString());//Banana,Orange,Apple,Mango
+
