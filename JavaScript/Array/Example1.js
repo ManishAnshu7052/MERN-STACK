77d7d2669f9e53c1380a40a8a72980bb03505fb2
Manish <patelmanish5272@gmail.com>
2023-01-05 02:03:16 -0800

    Array1

---
+++ JavaScript/Array/Example1.js
@@ -0,0 +1,16 @@
+const cars = ["Saab","Volvo","BMW"];
+console.log(cars);//[ 'Saab', 'Volvo', 'BMW' ]
+
+//Next 
+const cars2 = [
+    "Saab",
+    "Volvo",
+    "BMW"
+];//[ 'Saab', 'Volvo', 'BMW' ]
+
+//You can create a Array and then provide the element
+const cars3 =[];
+cars3[0]="BMW";
+cars3[1]="Saab";
+cars3[2]="Volvo";
+console.log(cars3);
