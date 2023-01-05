15168f2a811910f5e40b1486a7b42f18c153fc09
Manish <patelmanish5272@gmail.com>
2023-01-05 06:25:48 -0800

    pop

---
+++ JavaScript/Array/pop.js
@@ -0,0 +1,12 @@
+//pop() method remove the element at the last index. It returns the remove element
+
+let fruits = ["Apple","Mango","Grapes"];
+console.log(fruits);//[ 'Apple', 'Mango', 'Grapes' ]
+fruits.pop();
+console.log(fruits);//[ 'Apple', 'Mango' ]
+
+
+//Return remove element
+
+let poppedFruit = fruits.pop();
+console.log("popped fruits is",poppedFruit);
