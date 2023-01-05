09fe84a3d5c661d7c375227b0364a3d205d4a3b7
Manish <patelmanish5272@gmail.com>
2023-01-05 01:46:04 -0800

    Introduction Of Array

---
+++ JavaScript/Array/IntroToArray.js
@@ -0,0 +1,16 @@
+/* *An Array is a special variable , which can hold more one value;
+Example-- const cars = ["Saab","Value","BMW"];
+
+Why Use Array?-
+               If you hava a list of items (a list of car names, for example),storing the cars in a single
+               variable could look like this
+   let car1 = "Saab";
+   let car2 = "Volvo";
+   let car3 = "BMW";
+   
+Creating an Array-
+                  Using an Array literal is the easiest way to creating a Javascript Array;
+Syntax:
+const array_name = [item1,item2,....];                  
+ * 
+ */
