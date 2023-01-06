768be629fb9bdd9a3483cf8d4d3f938fb8785804
Manish <patelmanish5272@gmail.com>
2023-01-06 07:08:38 -0800

    push

---
+++ JavaScript/Array/push.js
@@ -0,0 +1,9 @@
+//push() method add the element last in the last index:
+
+let fruits = ["Apple","Mango","Grapes"];
+console.log(fruits);//[ 'Apple', 'Mango', 'Grapes' ]
+
+fruits.push("Banana");
+console.log(fruits);//[ 'Apple', 'Mango', 'Grapes', 'Banana' ]
+
+
