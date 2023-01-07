29e819eef921dbf0eb3fcd996c89d0f27ef1029e
Manish <patelmanish5272@gmail.com>
2023-01-07 01:55:22 -0800

    unshift

---
+++ JavaScript/Array/unshift.js
@@ -0,0 +1,6 @@
+//unshift() method add the element in the starting index[0];
+
+let fruits =["Apple","Mango","Grapes"];
+console.log(fruits);//[ 'Apple', 'Mango', 'Grapes' ]
+fruits.unshift("banana");
+console.log(fruits);//[ 'banana', 'Apple', 'Mango', 'Grapes' ]
