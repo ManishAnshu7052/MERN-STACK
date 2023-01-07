287f729a2acf1ea8bd896a2ff439bf0f363be2cf
Manish <patelmanish5272@gmail.com>
2023-01-07 02:29:37 -0800

    concat

---
+++ JavaScript/Array/concat.js
@@ -0,0 +1,7 @@
+//The concat() method creates a new array by mergin(concating) existing arrays
+
+const myGirls=["Cecils","Lone"];
+const myBoys=["Emil","Tonny","Ronny"];
+
+const myChildren = myGirls.concat(myBoys);
+console.log(myChildren);
