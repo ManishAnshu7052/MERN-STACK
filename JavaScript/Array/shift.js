cf73f4339814e6d87a3538d4d818a3904ed86fda
Manish <patelmanish5272@gmail.com>
2023-01-07 02:04:41 -0800

    shift

---
+++ JavaScript/Array/shift.js
@@ -0,0 +1,7 @@
+//shift() shift remove the starting[0] index;
+
+const fruits = ["apple","mango","banana"];
+console.log(fruits);//[ 'apple', 'mango', 'banana' ]
+
+fruits.shift();
+console.log(fruits);//[ 'mango', 'banana' ]
