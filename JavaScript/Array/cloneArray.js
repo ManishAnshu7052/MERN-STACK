646856e22a601f2c499318414e583d2da86ef833
Manish <patelmanish5272@gmail.com>
2023-01-07 02:09:17 -0800

    cloneArray

---
+++ JavaScript/Array/cloneArray.js
@@ -0,0 +1,5 @@
+let array1 = ["item1","item2"];
+let array2 = ["item3","item4"];
+array1.push("item5");
+console.log(array1);//[ 'item1', 'item2', 'item5' ]
+console.log(array2);//[ 'item3', 'item4' ]
