3644d70a814684dcac6f259f69157485059be41d
Manish <patelmanish5272@gmail.com>
2023-01-07 19:58:12 -0800

    reverse

---
+++ JavaScript/Array/reversing.js
@@ -0,0 +1,5 @@
+//The reverse() method reverse the element in an array;
+
+const fruits = ["Banana","Orange","Apple","Mango"];
+fruits.reverse();
+console.log(fruits);//[ 'Mango', 'Apple', 'Orange', 'Banana' ]
