59853164e84f436b46ea3f0871f39695884e6ec2
Manish <patelmanish5272@gmail.com>
2023-01-07 19:55:03 -0800

    sort

---
+++ JavaScript/Array/sort.js
@@ -0,0 +1,8 @@
+//The sort() method sorts an array alphabetically,
+
+const fruits = ["Banana","Orange","Apple","Mango"];
+fruits.sort();
+console.log(fruits);//[ 'Apple', 'Banana', 'Mango', 'Orange' ]
+
+const num1 = [12,45,1,0,67];
+console.log(num1.sort());//[ 0, 1, 12, 45, 67 ]
