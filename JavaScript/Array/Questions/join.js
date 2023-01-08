865d033fb8df4936b03e7dee8b39caf8d1449272
Manish <patelmanish5272@gmail.com>
2023-01-07 20:27:30 -0800

    joinArray

---
+++ JavaScript/Array/Questions/join.js
@@ -0,0 +1,12 @@
+//Write a program to join all element of the following array in String;
+
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+'));
+
+/**Output-
+ * Red,Green,White,Black
+   Red,Green,White,Black
+   Red+Green+White+Black
+ */
