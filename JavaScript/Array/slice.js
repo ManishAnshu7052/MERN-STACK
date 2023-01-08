bbac28b7a57783be4dd8c2b22eec09344e1fc97a
Manish <patelmanish5272@gmail.com>
2023-01-07 19:48:36 -0800

    slice

---
+++ JavaScript/Array/slice.js
@@ -0,0 +1,6 @@
+//The slice() method slices out a piece of the an array into a new array.
+
+const fruits = ["Banana","Orange","Lemon","Apple","Mango"];
+console.log(fruits.slice(1));
+
+//[ 'Orange', 'Lemon', 'Apple', 'Mango' ]
