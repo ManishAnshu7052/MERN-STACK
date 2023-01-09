74bc5716b171c3502ba8113b09309530ab595fc2
Manish <patelmanish5272@gmail.com>
2023-01-09 07:26:19 -0800

    SumAndAverage

---
+++ JavaScript/Array/Questions/AverageAndSum.js
@@ -0,0 +1,12 @@
+//Find the average and sum of the Array
+
+var nums =[1,2,3,4,5,6];
+var l = nums.length;
+var totalSum = 0;
+for(var i=0;i<nums.length;i++){
+    totalSum = totalSum + nums[i];
+}
+console.log(totalSum);//21
+
+var avg = totalSum/l;
+console.log("Average " + avg);//Average 3.5
