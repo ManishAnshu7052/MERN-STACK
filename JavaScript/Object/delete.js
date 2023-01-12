5d75609bf1fefd73f39981713ea1278f5f3c9339
Manish <patelmanish5272@gmail.com>
2023-01-12 05:09:20 -0800

    delete

---
+++ JavaScript/Object/delete.js
@@ -0,0 +1,10 @@
+const person ={
+    firstName : "John",
+    lastName : "Doe",
+    age : 23,
+    eyeColor : "blue",
+}
+delete person.age;
+console.log(person);
+
+//{ firstName: 'John', lastName: 'Doe', eyeColor: 'blue' }
