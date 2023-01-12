e2593dbf23706e091d6c79395ea13316958b2e4e
Manish <patelmanish5272@gmail.com>
2023-01-12 04:12:43 -0800

    obj

---
+++ JavaScript/Object/obj1.js
@@ -0,0 +1,9 @@
+const person={
+    firstName : "Manish",
+    lastName : "Patel",
+    age : 23,
+    eyeColor : "black",
+};
+console.log(person);
+
+//{ firstName: 'Manish', lastName: 'Patel', age: 23, eyeColor: 'black' }
