b53deb581b31f847dae3446ab9c3ce09c91c301d
Manish <patelmanish5272@gmail.com>
2023-01-12 04:27:34 -0800

    AccessData from Object

---
+++ JavaScript/Object/AccessData.js
@@ -0,0 +1,8 @@
+const person = {
+    firstName : "Manish",
+    lastName : "Patel",
+    age : 23,
+};
+console.log(person.firstName);//Manish
+console.log(person.firstName +  " is " + person.age + " years old");
+//Manish is 23 years old
