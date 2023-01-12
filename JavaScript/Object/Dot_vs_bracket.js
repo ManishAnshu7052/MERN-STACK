dc07e9c44435d92d3131088b468ee470561ac9a0
Manish <patelmanish5272@gmail.com>
2023-01-12 05:05:30 -0800

    Dot vs Blacket

---
+++ JavaScript/Object/Dot_vs_bracket.js
@@ -0,0 +1,7 @@
+const person = {
+    Name : "Manish Kumar Patel",
+    age : 23,
+    "person Hobbies" :["guiter","sleeping"],
+}
+//console.log(person.person Hobbies); dot cannot be access this type
+console.log(person["person Hobbies"]);
