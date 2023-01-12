5a84c3f018dde899b6ba1dc9a7161e513a0dd367
Manish <patelmanish5272@gmail.com>
2023-01-12 04:49:21 -0800

    Add key value

---
+++ JavaScript/Object/Addkey.js
@@ -0,0 +1,17 @@
+const person={
+    firstName : "Manish",
+    lastName : "Patel",
+    age : 23,
+    eyeColor : "black",
+};
+person.gender = "Male";
+console.log(person);
+
+const person2 = {
+    Name : "John",
+    lastName : "Doe",
+    age : 34,
+    eyeColor : "blue",
+}
+person2.nationality = "English";
+console.log(person2);
