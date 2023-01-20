bd3b2031c3f025a33b5ed2495d55c5250ece829d
Manish <patelmanish5272@gmail.com>
2023-01-19 17:01:52 -0800

    objects

---
+++ JavaScript/Object/Questions/Question1.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to list the properties of a JavaScript object
+
+var student = {
+    name : "David Rayy",
+    class : 6,
+    age : 12,
+    roll_no : 23,
+}
+console.log(student);
