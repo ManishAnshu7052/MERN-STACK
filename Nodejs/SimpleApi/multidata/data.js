33a13d7c05b8652cb32afdabe3bc2a2366d1d0a7
Manish <patelmanish5272@gmail.com>
2023-03-01 17:07:56 -0800

    multidata

---
+++ Nodejs/SimpleApi/multidata/data.js
@@ -0,0 +1,6 @@
+const data=[
+    {name:"Manish kumar patel",email:"patelmanish5252@gmail.com",Address:"Prayagrag",Phone:9118997628},
+
+    {name:"Avanish kumar patel",email:"patelmanish5272@gmail.com",Address:"Prayagrag",Phone:7052575272}
+]
+module.exports =data;
