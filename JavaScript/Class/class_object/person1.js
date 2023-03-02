147b9acf91b50a2bcc914d757bc43a66fe9c9ffa
Manish <patelmanish5272@gmail.com>
2023-03-02 06:31:06 -0800

    objects

---
+++ JavaScript/Class/class_object/person1.js
@@ -0,0 +1,10 @@
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+}
+//creating objects
+const person1 = new Person('John');
+const person2 = new Person('Jack');
+console.log(person1.name);
+console.log(person2.name);
---
+++ JavaScript/Class/class_object/rectangle.js
@@ -0,0 +1,11 @@
+class Rectangle{
+    constructor(height,width){
+        this.height = height;
+        this.width = width;
+    }
+}
+//create an objects
+const a = new Rectangle(100,20);
+console.log(a.height)
+console.log(a.width)
+
