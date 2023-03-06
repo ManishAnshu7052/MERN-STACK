afcfd0aba448aec9a2d1f974d9916f37eb10bd1d
Manish <patelmanish5272@gmail.com>
2023-03-05 18:22:30 -0800

    class-person2

---
+++ JavaScript/Class/Basic-Class/persons/person2.js
@@ -0,0 +1,9 @@
+class Person{
+    constructor(n,a){
+        this.name = n;
+        this.age = a;
+    }
+}
+let person2 = new Person("Manish Kumar Patel",24);
+console.log(person2);//Person { name: 'Manish Kumar Patel', age: 24 }
+console.log(person2.name);//Manish Kumar Patel
