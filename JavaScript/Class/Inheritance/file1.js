9d93c02a10f2cd9732e37d8748249051df033d47
Manish <patelmanish5272@gmail.com>
2023-03-05 18:46:32 -0800

    inheritance

---
+++ JavaScript/Class/Inheritance/file1.js
@@ -0,0 +1,10 @@
+class employes{
+    constructor(){
+        console.log("emp constructor callled.....");
+    }
+}
+class manager extends employes{
+
+}
+let manager1 = new manager();
+//emp constructor callled.....
