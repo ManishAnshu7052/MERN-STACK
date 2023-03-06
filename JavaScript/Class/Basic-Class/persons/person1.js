028de247f5962dac895ba21b95a93b43c8b08ed6
Manish <patelmanish5272@gmail.com>
2023-03-05 18:18:10 -0800

    class_person1

---
+++ JavaScript/Class/Basic-Class/persons/person1.js
@@ -0,0 +1,12 @@
+class Person{
+    constructor(){
+        console.log("person created.....");
+    }
+}
+let person1 = new Person();
+console.log(person1);
+
+/*output-
+person created.....
+Person {}
+*/
