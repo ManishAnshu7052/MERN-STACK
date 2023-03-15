10526fa1e838debc19639307e2b2a18cde80743e
Manish <patelmanish5272@gmail.com>
2023-03-15 06:00:06 -0700

    Parameter and Argument

---
+++ JavaScript/Function/fun/file2.js
@@ -0,0 +1,15 @@
+function mulTable(num){
+    for(i=1;i<=10;i++){
+        document.write(`${num} x ${i} = ${num*i}`);
+        document.write("<br>")
+    }
+}
+mulTable(12);
+
+document.write("<br>")
+
+function add(num1,num2){
+     document.write(num1 + num2);
+
+}
+add(5,9);
