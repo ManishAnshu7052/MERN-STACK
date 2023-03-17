841e9ceb727acac2a50b506bf76f7f2be6ce43cd
Manish <patelmanish5272@gmail.com>
2023-03-17 09:42:24 -0700

    object property

---
+++ JavaScript/Object/OBJ/file1.js
@@ -0,0 +1,15 @@
+//object property
+ let person = {
+    firstName: "Manish",
+    lastName: "Patel",
+ }
+ console.log(person);
+ console.log(person.firstName);
+ console.log(person['lastName']);
+
+ //space
+ let person2 = {
+    'first Name':'Avanish',
+    lasrName: 'Patel',
+ }
+ console.log(person2['first Name'])
