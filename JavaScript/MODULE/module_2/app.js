3027c8da63b7ad7333da651d5ad8fdfef8ccbc85
Manish <patelmanish5272@gmail.com>
2023-04-10 06:19:11 -0700

    module-2

---
+++ JavaScript/MODULE/module_2/app.js
@@ -0,0 +1,9 @@
+//user.js
+import { name } from "./module/user.js";
+console.log(name);
+import { age } from "./module/user.js";
+console.log(age);
+import { email } from "./module/user.js";
+console.log(email);
+import { address } from "./module/user.js";
+console.log(address)
---
+++ JavaScript/MODULE/module_2/module/user.js
@@ -0,0 +1,4 @@
+export let name = "Avanish Kumar Patel";
+export let age = 20;
+export let email = "avanish@gmail.com";
+export let address = "Prayagrag";
